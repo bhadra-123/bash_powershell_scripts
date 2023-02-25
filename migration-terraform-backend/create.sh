9d85468aae71f4538bc430b1705c0a0b0a93e141
bhadra-123 <bhadra.mangunuru@gmail.com>
2023-02-25 17:27:13 +0000

    test

--- migration-terraform-backend/create.sh
+++ migration-terraform-backend/create.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-export subscription_id="f5db7161-4296-45e8-96e3-e67567aa1b22"
-export resource_group_name="terraform-storage-backend-rg" 
-export storage_account_name="backendstorage63826482"
-export storage_container_1_name="dev-backend-container"
-export storage_container_2_name="qa-backend-container"
-export storage_container_3_name="prod-backend-container"
+export subscription_id="dc87d979-80e5-489a-915e-b5faaf6d62bc"
+export location="eastus"
+export resource_group_name="terraform-backend-rg" 
+export storage_account_name="terraformbackendstac743"
+export hub_storage_account_name="hub-stct"
+export spoke_storage_account_name="spoke-stct"
 
 echo "-----------------------"
 echo  setting up subscripton
@@ -19,24 +19,22 @@ echo "------------------------"
 echo  creating resource group
 echo "------------------------"
 
-az group create --name $resource_group_name --location centralus
+az group create --name $resource_group_name --location $location
 
-az group update --name $resource_group_name --set tags.owner=bhadra tags.source=manual tags.location=centralus
+az group update --name $resource_group_name --set tags.owner=bhadra tags.source=manual tags.location=$location
 
 echo "-------------------------"
 echo  creating storage account
 echo "-------------------------"
 
-az storage account create --name $storage_account_name --resource-group $resource_group_name --location centralus --sku Standard_LRS --encryption-services blob
+az storage account create --name $storage_account_name --resource-group $resource_group_name --location $location --sku Standard_LRS --encryption-services blob
 
-az storage account update --name $storage_account_name --resource-group $resource_group_name --set tags.owner=bhadra tags.source=manual tags.location=centralus
+az storage account update --name $storage_account_name --resource-group $resource_group_name --set tags.owner=bhadra tags.source=manual tags.location=$location
 
 echo "---------------------------"
 echo  creating storage container
 echo "---------------------------"
 
-az storage container create --name $storage_container_1_name --account-name $storage_account_name --auth-mode login
+az storage container create --name $hub_storage_account_name --account-name $storage_account_name --auth-mode login
 
-az storage container create --name $storage_container_2_name --account-name $storage_account_name --auth-mode login
-
-az storage container create --name $storage_container_3_name --account-name $storage_account_name --auth-mode login
+az storage container create --name $spoke_storage_account_name --account-name $storage_account_name --auth-mode login
